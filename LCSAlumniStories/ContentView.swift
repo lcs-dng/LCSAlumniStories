76107bc58e2311536a4cc0242a05e72f104114a4
Dexter Ng <DNg@lcs.on.ca>
2025-01-08 11:19:26 -0500

    Initial Commit

---
+++ LCSAlumniStories/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LCSAlumniStories
+//
+//  Created by Dexter Ng on 2025-01-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LCSAlumniStories/LCSAlumniStoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LCSAlumniStoriesApp.swift
+//  LCSAlumniStories
+//
+//  Created by Dexter Ng on 2025-01-08.
+//
+
+import SwiftUI
+
+@main
+struct LCSAlumniStoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
