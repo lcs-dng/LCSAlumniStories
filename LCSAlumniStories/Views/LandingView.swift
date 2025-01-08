a01ddba18f0bd96d80cd5f0bd1424e15d840b62e
Dexter Ng <DNg@lcs.on.ca>
2025-01-08 11:27:15 -0500

    Organized files.

---
+++ LCSAlumniStories/Views/LandingView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LCSAlumniStories
+//
+//  Created by Dexter Ng on 2025-01-08.
+//
+
+import SwiftUI
+
+struct LandingView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    LandingView()
+}
